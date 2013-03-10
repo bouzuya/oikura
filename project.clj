901a952cca861c9739d4731c587907703c4b5583
bouzuya <bouzuya@gmail.com>
2013-03-11 02:25:49 +0900

    added chart.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.1"]
+                 [jfree/jfreechart "1.0.13"]
                  [clj-http "0.6.5"]]
   :main oikura)
 

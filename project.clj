57fe4cdb3ffa1e4031bff92759a5e7db53d817ed
bouzuya <bouzuya@gmail.com>
2013-03-11 03:57:43 +0900

    added storage.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.zip "0.1.1"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
                  [jfree/jfreechart "1.0.13"]
                  [clj-http "0.6.5"]]
   :main oikura)
